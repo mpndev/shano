2c8fdc8d610f30cff98e1e62a1b6e0e65f137fe2
martin <martin.nikolov@ffwagency.com>
2019-10-09 18:28:40 +0300

    Refactoring AddTicketToCartForm

--- web/modules/custom/shano_shopping_cart/src/Form/AddTicketToCartForm.php
+++ web/modules/custom/shano_shopping_cart/src/Form/AddTicketToCartForm.php
@@ -6,55 +6,53 @@ use Drupal\node\Entity\Node;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Ajax\AjaxResponse;
 use Drupal\Core\Ajax\ReplaceCommand;
-use Drupal\Core\Ajax\OpenDialogCommand;
 use Drupal\paragraphs\Entity\Paragraph;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Ajax\OpenModalDialogCommand;
 
 class AddTicketToCartForm extends FormBase {
 
-  protected $form_id = NULL;
-  protected $node_id = NULL;
+  private $form_id = NULL;
+  private $event = NULL;
+  private $tickets = NULL;
+  private $shopping_cart = NULL;
+  private $have_tickets_for_this_event = FALSE;
+  private $wasTicketAdded = FALSE;
 
-  public function __construct($node_id) {
-    $this->form_id = 'shano_shopping_cart_form' . $node_id;
-    $this->node_id = $node_id;
+  /**
+   * AddTicketToCartForm constructor.
+   *
+   * @param $event_id
+   */
+  public function __construct($event_id) {
+    $this->form_id = 'shano_shopping_cart_form' . $event_id;
+    $this->event = Node::load($event_id);
+    $this->tickets = Paragraph::load($this->event->get('field_tickets')->getValue()[0]['target_id']);
+    $this->shopping_cart = \Drupal::service('tempstore.private')->get('shopping_cart');
   }
 
   /**
-   * Returns a unique string identifying the form.
-   *
-   * The returned ID should be a unique string that can be a valid PHP function
-   * name, since it's used in hook implementation names such as
-   * hook_form_FORM_ID_alter().
-   *
-   * @return string
-   *   The unique string identifying the form.
+   * @return string|null
    */
   public function getFormId() {
     return $this->form_id;
   }
 
   /**
-   * Form constructor.
-   *
    * @param array $form
-   *   An associative array containing the structure of the form.
    * @param \Drupal\Core\Form\FormStateInterface $form_state
-   *   The current state of the form.
    *
    * @return array
-   *   The form structure.
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $form['node_id'] = [
       '#type' => 'hidden',
-      '#value' => $this->node_id,
+      '#value' => $this->event->id(),
     ];
     $form['submit'] = [
       '#type' => 'submit',
       '#value' => $this->t('Shut up and take my money!'),
-      '#wrapper' => 'message_box' . $this->node_id,
+      '#wrapper' => 'message_box' . $this->event->id(),
       '#ajax' => [
         'callback' => '::processTicketAdding',
         'disable-refocus' => FALSE,
@@ -69,59 +67,69 @@ class AddTicketToCartForm extends FormBase {
     return $form;
   }
 
+  /**
+   * @param array $form
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $event = Node::load($this->node_id);
-    $tickets_id = $event->get('field_tickets')->getValue()[0]['target_id'];
-    $tickets = Paragraph::load($tickets_id);
-    $quantity = $tickets->get('field_quantity')->getString();
-
-    if ($quantity > 0) {
-      $order_already_exist = TRUE;
-      $shopping_cart = \Drupal::service('tempstore.private')->get('shopping_cart');
-      $shopping_cart_tickets = $shopping_cart->get('tickets');
-
-      foreach ($shopping_cart_tickets as &$shopping_cart_ticket) {
-        if ($shopping_cart_ticket['event_id'] === $this->node_id) {
-          $shopping_cart_ticket['tickets_quantity']++;
-          $order_already_exist = FALSE;
-        }
-      }
-      if ($order_already_exist === TRUE) {
-        $shopping_cart_tickets[] = [
-          'event_id' => $this->node_id,
-          'tickets_quantity' => 1,
-        ];
-      }
-      $shopping_cart->set('tickets', $shopping_cart_tickets);
-      $form['was_ticket_reserved'] = TRUE;
-    } else {
-      $form['was_ticket_reserved'] = FALSE;
+    if ($this->haveAvailableTickets()) {
+      $this->addTicket();
     }
   }
 
+  /**
+   * @param array $form
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *
+   * @return \Drupal\Core\Ajax\AjaxResponse
+   */
   public function processTicketAdding(array &$form, FormStateInterface $form_state) {
-    $markup = $form['was_ticket_reserved'] ? 'Ticket was added to your shopping cart.' : 'No more tickets left.';
     $elem = [
-      '#type' => 'textfield',
+      '#type' => 'text',
       '#size' => '60',
       '#disabled' => TRUE,
-      '#value' => "I am a new textfield: $selectedText!",
+      '#value' => "",
       '#attributes' => [
-        'id' => ['edit-output'],
+        'id' => ['message_box' . $this->event->id()],
       ],
     ];
-    $renderer = \Drupal::service('renderer');
-    $renderedField = $renderer->render($elem);
 
     $dialogText['#attached']['library'][] = 'core/drupal.dialog.ajax';
-    $dialogText['#markup'] = $markup;
+    $dialogText['#markup'] = $this->wasTicketAdded ? 'Ticket was added to your shopping cart.' : 'No more tickets left.';
 
     $response = new AjaxResponse();
-    $response->addCommand(new ReplaceCommand('#message_box' . $this->node_id, $renderedField));
+    $response->addCommand(new ReplaceCommand('#message_box' . $this->event->id(), \Drupal::service('renderer')->render($elem)));
     $response->addCommand(new OpenModalDialogCommand('Info About Your Request...', $dialogText, ['width' => '300']));
 
-    // Finally return the AjaxResponse object.
     return $response;
+  }
+
+  /**
+   * Add Ticket to the Shopping Cart
+   */
+  private function addTicket () {
+    $tickets = $this->shopping_cart->get('tickets');
+    foreach ($tickets as &$ticket) {
+      if ($ticket['event_id'] === $this->event->id()) {
+        $ticket['tickets_quantity']++;
+        $this->have_tickets_for_this_event = TRUE;
+      }
+    }
+    if ($this->have_tickets_for_this_event === FALSE) {
+      $tickets[] = [
+        'event_id' => $this->event->id(),
+        'tickets_quantity' => 1,
+      ];
+    }
+    $this->shopping_cart->set('tickets', $tickets);
+    $this->wasTicketAdded = TRUE;
+  }
+
+  /**
+   * @return bool
+   */
+  private function haveAvailableTickets() {
+    return $this->tickets->get('field_quantity')->getString() > 0;
   }
 
 }
