f9f70024af21dc94328c48577cce58a2db7631b9
martin <martin.nikolov@ffwagency.com>
2019-10-17 10:25:41 +0300

    update stripe session on tickets reducing.

--- web/modules/custom/shano_shopping_cart/src/Form/ReduceTicketForm.php
+++ web/modules/custom/shano_shopping_cart/src/Form/ReduceTicketForm.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\shano_shopping_cart\Form;
 
+use Drupal\Core\Url;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Ajax\HtmlCommand;
 use Drupal\Core\Ajax\AjaxResponse;
@@ -9,6 +10,7 @@ use Drupal\Core\Ajax\ReplaceCommand;
 use Drupal\Core\Ajax\RedirectCommand;
 use Drupal\Core\Ajax\SettingsCommand;
 use Drupal\shano_shopping_cart\Event;
+use Drupal\shano_shopping_cart\Order;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\shano_shopping_cart\ShanoStripe;
 use Drupal\shano_shopping_cart\ShoppingCart;
@@ -80,30 +82,42 @@ class ReduceTicketForm extends FormBase {
    * @param \Drupal\Core\Form\FormStateInterface $form_state
    *
    * @return \Drupal\Core\Ajax\AjaxResponse
+   * @throws \Stripe\Exception\ApiErrorException
    */
   public function processTicketReducing(array &$form, FormStateInterface $form_state) {
     $shopping_cart = new ShoppingCart();
     $shopping_cart->removeTicketForEvent($this->event);
+    $response = new AjaxResponse();
+
+    if ($shopping_cart->isEmpty()) {
+      $url = Url::fromRoute('shano_shopping_cart.index')->toString();
+      $response->addCommand(new RedirectCommand($url));
+      return $response;
+    }
+
     $quantity = $shopping_cart->getTicketsQuantityForEvent($this->event);
-    $quantity_span = "<span id='tickets-quantity-" . $this->event->id() . "'>" . $quantity . "</span>";
+
+    if ($quantity) {
+      $quantity_span = "<span id='tickets-quantity-" . $this->event->id() . "'>" . $quantity . "</span>";
+      $response->addCommand(new ReplaceCommand('#tickets-quantity-' . $this->event->id(), $quantity_span));
+    } else {
+      $nothing = '';
+      $response->addCommand(new ReplaceCommand('#main-block-' . $this->event->id(), $nothing));
+    }
 
     $dialog_text['#attached']['library'][] = 'core/drupal.dialog.ajax';
     $dialog_text['#markup'] = t('Ticket was removed from your Shopping Cart!');
-
-    $response = new AjaxResponse();
-    if ($quantity) {
-      $response->addCommand(new ReplaceCommand('#tickets-quantity-' . $this->event->id(), $quantity_span));
-    } else {
-      $response->addCommand(new ReplaceCommand('#main-block-' . $this->event->id(), ''));
-      if ($shopping_cart->isEmpty()) {
-        $response->addCommand(new RedirectCommand("http://shano.local/shopping-cart"));
-        return $response;
-      }
-    }
-
-    //$response->addCommand(new SettingsCommand ($data));
-    $response->addCommand(new HtmlCommand('#stripe-button', t('Buy the tickets for:') . ' ' . $shopping_cart->getTotal() . '$'));
     $response->addCommand(new OpenModalDialogCommand($this->event->title, $dialog_text, ['width' => '300']));
+
+    $order = new Order();
+    $shano_stripe = new ShanoStripe();
+    $order->prepare($shopping_cart);
+    $shano_stripe->prepare($order->get());
+
+    $updated_drupalSettings = ['stripe_session_id' => $shano_stripe->session->id,];
+
+    $response->addCommand(new SettingsCommand ($updated_drupalSettings, TRUE));
+    $response->addCommand(new HtmlCommand('#stripe-button', t('Buy the tickets for:') . ' ' . $shopping_cart->getTotal() . '$'));
 
     return $response;
   }
